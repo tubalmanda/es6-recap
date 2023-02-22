16b263e58a1ba6cdfe8438198446f57d428257b3
Tubal Manda <Tubal Manda>
2023-02-22 20:20:47 +0600

    setup

---
+++ recap.js
@@ -0,0 +1,36 @@
+/* 
+ Mush know
+ 1. var, let, const (already practices)
+ 2. template string
+    2.1 create multi-link string ('')
+    2.2 add dynamic variable (expression): '${}'
+3. Arrow function 
+    3.1 single parameter arrow function
+    3.2 two parameters arrow function
+    3.3 arrow function with function body. 
+    and don't forget you have to return explicitly 
+4. map, filter, find
+5. optional chane
+
+*/
+
+// Good to have
+/*
+    1. spread operator: [...numbers, 12, 45]
+    2. destructuring 
+        2.1: object destructuring. how to get property name as variable 
+            const {x} = {x: 3, y:5}
+        2.2: array destructuring 
+            const [a, b] = [3, 8, 12, 56]
+    3. default parameter 
+    4. forEach
+    5. dot notation vs bracket notation 
+*/
+
+//  Should know the name
+/*
+1. reduce 
+2. inheritane
+3. prototypical chain 
+4. class, object
+*/
